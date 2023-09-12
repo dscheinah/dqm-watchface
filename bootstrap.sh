411b6970ddd4e1ddc0656bce14d3791a7c467e21
dscheinah <github@jb.chmann.net>
2023-09-12 22:51:41 +0200

    add strip to convert

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,9 +30,9 @@ find $prefix -type f | while read -r file; do
   fileColor="${file%.*}~color.png"
   fileBw="${file%.*}~bw.png"
   if [[ $hasConvert ]]; then
-    convert "$file" -sample 44x\> "$file"
-    convert "$file" -flatten -alpha off +dither -remap ./resources/pebble_colors_64.gif -trim -type palette "$fileColor"
-    convert "$file" -flatten -alpha off -monochrome -trim -colors 2 -type palette "$fileBw"
+    convert "$file" -strip -sample 44x\> "$file"
+    convert "$file" -strip -flatten -alpha off +dither -remap ./resources/pebble_colors_64.gif -trim -type palette "$fileColor"
+    convert "$file" -strip -flatten -alpha off -monochrome -trim -colors 2 -type palette "$fileBw"
   else
     cp "$file" "${file%.*}~color.png"
     cp "$file" "${file%.*}~bw.png"
