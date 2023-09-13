496e78f93c1f36f5a93f4440a4a8c1a7784973b0
dscheinah <github@jb.chmann.net>
2023-09-13 21:06:23 +0200

    set default clear text background

--- app/src/c/render/helper.c
+++ app/src/c/render/helper.c
@@ -4,6 +4,7 @@
 TextLayer* helper_create_text_layer(Layer* parent, GRect rect, char* font, GTextAlignment alignment) {
   TextLayer* layer = text_layer_create(rect);
   layer_add_child(parent, text_layer_get_layer(layer));
+  text_layer_set_background_color(layer, GColorClear);
   text_layer_set_font(layer, fonts_get_system_font(font));
   text_layer_set_text_alignment(layer, alignment);
   return layer;
