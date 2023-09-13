1e74b2657f84b154061fb65cc6d867e4d851d8ab
dscheinah <github@jb.chmann.net>
2023-09-13 21:06:09 +0200

    micro optimize initial bounds

--- app/src/c/render/layout.c
+++ app/src/c/render/layout.c
@@ -2,17 +2,14 @@
 #include "layout.h"
 #include "helper.h"
 
+#define X (PBL_DISPLAY_WIDTH - 144) / 2
+#define Y (PBL_DISPLAY_HEIGHT - 168) / 2
+
 static Layer* rootLayer;
 
 void layout_load(Window* window) {
   Layer* root = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(root);
-
-  int x = (bounds.size.w - 144) / 2;
-  int y = (bounds.size.h - 168) / 2;
-  GRect coords = GRect(x, y, 144, 168);
-
-  rootLayer = helper_create_layer(root, coords);
+  rootLayer = helper_create_layer(root, GRect(X, Y, 144, 168));
 }
 
 Layer* layout_get_root() {
